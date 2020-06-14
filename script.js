5416d2f535fc3c25d220565fcaec4bcce3f33db2
Алексей Дубовко <alexey-dubovko@MacBook-Pro-Moryarty.local>
2020-06-14 20:05:15 +0500

    ДЗ для 1 урока

---
+++ script.js
@@ -0,0 +1,7 @@
+let tc = parseInt(prompt('Введите температуру по Цельсию:'));
+let tf = (9 / 5) * tc + 32;
+alert("Температура по Фарингейту: " + tf);
+
+let name = "Василий";
+let admin = name;
+alert(admin);
