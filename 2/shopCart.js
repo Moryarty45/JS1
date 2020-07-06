62c779abe80e2af13e4c85efc6c37e6cd4ef16f8
Алексей Дубовко <alexey-dubovko@MacBook-Pro-Moryarty.local>
2020-07-06 13:59:23 +0500

    ДЗ для 6го урока (корзина)

--- 2/shopCart.js
+++ 2/shopCart.js
@@ -1,3 +1,4 @@
+function init() {
 var desc = '<div class=itemshop>';
 for (var i = 0; i < items.length; i++) {
     desc+='<div class=item>';
@@ -5,9 +6,30 @@ for (var i = 0; i < items.length; i++) {
     desc+=items[i].name+'<br>';
     desc+='Цена: '+items[i].price+'<br>';
     desc+='<button class=toCart data-id='+i+'>В корзину</button>';
+    itemBtn = document.getElementById("button");
+    itemBtn.onclick = addItem;
     desc+='</div>';
 }
 desc += '</div>';
 document.getElementById('shop').innerHTML = desc;
+}
 
 var cart = [];
+var sum = 0;
+
+function addItem(obj){
+    var selectedItem = itemsArray[obj.target.id.split("_")[1]];
+    var selectedItems = document.getElementsByClassName("selected_items")[0];
+    var tr = selectedItems.insertRow(-1);
+    var td = tr.insertCell(-1);
+    td.appendChild(document.createTextNode(selectedItem.name));
+    var td = tr.insertCell(-1);
+    td.style.textAlign = "center";
+    td.style.width = "100px";
+    td.appendChild(document.createTextNode("Цена: " + selectedItem.price + "\u20bd"));
+
+    sum += selectedItem.price;
+    document.getElementsByClassName("sum")[0].textContent = "Sum: " + sum + "\u20bd";
+}
+
+window.onload = init;
